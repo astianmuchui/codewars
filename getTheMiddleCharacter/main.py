3d044ec14100d30a310fefd026da7236d87d3bae
Seb Astian <Sebastianmuchui79@gmail.com>
2021-07-14 10:58:01 +0300

    Update main.py

--- getTheMiddleCharacter/main.py
+++ getTheMiddleCharacter/main.py
@@ -6,7 +6,7 @@ if length%2 == 0:
     half = length/2
     start_point = half-1
     end_Point = half
-    print (text[int(start_point):int(end_Point)])
+    print (text[int(start_point):int(end_Point+1)])
 else:
     half = length/2
     char_pos = round(half,0)
